0634920dd5ed1d543d80b6ffaa5ee3649407b29a
nicochnz <chiche.nicolas@gmail.com>
2025-02-27 10:45:18 +0100

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "@emotion/react";
 import Homepage from "./components/Homepage";
+import Navbar from "./components/Navbar";
 import { theme } from "./theme";
 function App() {
 	return (
 		<ThemeProvider theme={theme}>
 			<main>
 				<Homepage />
+				<Navbar />
 			</main>
 		</ThemeProvider>
 	);
---
+++ src/assets/images/DislikeIcone.tsx
@@ -0,0 +1,18 @@
+export const DislikeIcon = () => (
+  <svg
+    viewBox="0 0 16 16"
+    xmlns="http://www.w3.org/2000/svg"
+    width="60"
+    height="60"
+  >
+    <g transform="translate(-52.11 -582.91)">
+      <title>J'aime pas</title>
+      <path
+        d="m64.792 582.91-4.6811 4.6811-4.6811-4.6811-3.3189 3.3189 4.6811 4.6811-4.6811 4.6811 3.3189 3.3189 4.6811-4.6811 4.6811 4.6811 3.3189-3.3189-4.6811-4.6811 4.6811-4.6811-3.3189-3.3189z"
+        fill="#ef3416"
+        stroke="#242124"
+        strokeWidth="0.3"
+      />
+    </g>
+  </svg>
+);
